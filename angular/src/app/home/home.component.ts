836cf89fd21e024f521ac0fb961ee40518909788
Keith Harvey <keith.harvey@nwpharma.com>
2022-12-20 11:56:30 -0500

    first commit

---
+++ angular/src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { AuthService } from '@abp/ng.core';
+import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  get hasLoggedIn(): boolean {
+    return this.oAuthService.hasValidAccessToken();
+  }
+
+  constructor(private oAuthService: OAuthService, private authService: AuthService) {}
+
+  login() {
+    console.log("Going to page...")
+    this.authService.navigateToLogin();
+  }
+}
---
+++ aspnet-core/src/Acme.BookStore.EntityFrameworkCore/Migrations/20221219195519_Fix_Book_Entity_Price_Column.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Acme.BookStore.Migrations
+{
+    public partial class Fix_Book_Entity_Price_Column : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Price",
+                table: "AppBooks",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "AppBooks");
+        }
+    }
+}
